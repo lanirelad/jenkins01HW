5a4d6760c453759486c09b3e3534407fa94a509f
lanirelad <lanirelad@gmail.com>
2024-07-17 22:20:44 +0300

    Added 'day of the week using numericals values

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import datetime as DT
 
 print(f"Today is - {DT.date.today()} and the time now is {DT.datetime.now().strftime("%H:%M:%S")}")
-print(f"today is the {DT.date.weekday(DT.datetime.today())+1} day out of 7 in the week")
+print(f"today is day num. {DT.date.weekday(DT.datetime.today())+1} out of 7 in the week")
